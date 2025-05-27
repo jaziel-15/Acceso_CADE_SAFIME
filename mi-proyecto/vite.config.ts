4f09560e0ea379d99bd55cc12657488f550bf577
jaziel-15 <jaziel15gamer@gmail.com>
2025-05-26 19:53:41 -0600

    Se agrega ajustes ejecutable

--- mi-proyecto/vite.config.ts
+++ mi-proyecto/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Asegúrate de esto para el despliegue en la raíz del dominio
+  build: {
+    outDir: 'dist' // Esto es lo que Vercel necesita
+  }
 })
+
